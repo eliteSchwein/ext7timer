86b0345db8e0b140e30d346366959d2a768e5ccc
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-02 11:43:37 +0200

    add icon path

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,5 +3,7 @@ defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Typo37timerExtension',
-    '7timerextensionplugin'
+    '7timerextensionplugin',
+    '7Timer Plugin',
+    'EXT:typo3_7timer_extension/Resources/Public/Icons/user_plugin_7timerextensionplugin.svg'
 );
