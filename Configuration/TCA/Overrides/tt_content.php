d1f923cbd0994de0e20b7d05ada7923c5a82f6e9
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-02 11:41:35 +0200

    remove faulty code

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,6 +3,5 @@ defined('TYPO3_MODE') || die();
 
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Typo37timerExtension',
-    '7timerextensionplugin',
-    '7Timer Plugin'
+    '7timerextensionplugin'
 );
