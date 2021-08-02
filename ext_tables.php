a704585e6338c944248b90ae0df8ec52a50dd3ad
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-02 11:49:30 +0200

    add flexform

--- ext_tables.php
+++ ext_tables.php
@@ -5,4 +5,11 @@ defined('TYPO3_MODE') || die();
 call_user_func(static function() {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_typo37timerextension_domain_model_configuration', 'EXT:typo3_7timer_extension/Resources/Private/Language/locallang_csh_tx_typo37timerextension_domain_model_configuration.xlf');
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo37timerextension_domain_model_configuration');
+
+    $pluginSignature = 'typo3_7timer_extension_7timerextensionplugin';
+    $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
+    ExtensionManagementUtility::addPiFlexFormValue(
+        $pluginSignature,
+        'FILE:EXT:typo3_7timer_extension/Configuration/FlexForms/Configuration.xml'
+    );
 });
