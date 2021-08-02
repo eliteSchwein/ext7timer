ddc778bc61a7c0d375a868d2712e3be55eb1f2fb
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-02 11:21:45 +0200

    change naming

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -4,5 +4,5 @@ defined('TYPO3_MODE') || die();
 \TYPO3\CMS\Extbase\Utility\ExtensionUtility::registerPlugin(
     'Typo37timerExtension',
     '7timerextensionplugin',
-    ''
+    'typo3_7timer_extension'
 );
