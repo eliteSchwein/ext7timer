8c3d409d0a55370a84fa0c9dd6a666d30278f00b
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-02 13:23:22 +0200

    reorder functions

--- ext_tables.php
+++ ext_tables.php
@@ -3,13 +3,13 @@
 defined('TYPO3_MODE') || die();
 
 call_user_func(static function() {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_typo37timerextension_domain_model_configuration', 'EXT:typo3_7timer_extension/Resources/Private/Language/locallang_csh_tx_typo37timerextension_domain_model_configuration.xlf');
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo37timerextension_domain_model_configuration');
-
     $pluginSignature = 'typo3_7timer_extension_7timerextensionplugin';
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
         $pluginSignature,
         'FILE:EXT:typo3_7timer_extension/Configuration/FlexForms/Configuration.xml'
     );
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_typo37timerextension_domain_model_configuration', 'EXT:typo3_7timer_extension/Resources/Private/Language/locallang_csh_tx_typo37timerextension_domain_model_configuration.xlf');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_typo37timerextension_domain_model_configuration');
 });
