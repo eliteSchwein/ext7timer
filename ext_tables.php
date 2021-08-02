ecaf4b35cbff6b2346bddd8ce08bc84f5405ec67
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-02 15:33:18 +0200

    replace key

--- ext_tables.php
+++ ext_tables.php
@@ -7,7 +7,7 @@ call_user_func(static function() {
     $GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
         $pluginSignature,
-        'FILE:EXT:typo3_7timer_extension/Configuration/FlexForms/Configuration.xml'
+        'FILE:EXT:7timer_extension/Configuration/FlexForms/Configuration.xml'
     );
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_7timerextension_domain_model_configuration', 'EXT:7timer_extension/Resources/Private/Language/locallang_csh_tx_7timerextension_domain_model_configuration.xlf');
